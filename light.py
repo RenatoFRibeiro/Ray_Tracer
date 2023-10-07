bbf4c353f123c5353e0ef3ea0f23ef0ccd207a68
RenatoFRibeiro <renarenato2008@gmail.com>
2023-10-07 11:25:04 +0100

    Update light.py

--- light.py
+++ light.py
@@ -1,6 +1,3 @@
-import pygame
-import sys
-
 class Light:
     def __init__(self, file_name_specifications):
         self.file_name_specifications = file_name_specifications
@@ -25,15 +22,14 @@ class Light:
                     light_intensity_flag = 2
                     continue
                 if light_transf_flag == 1 and light_intensity_flag == 2:
-                    self.light_intensity = parts[0]
+                    self.light_intensity = tuple(int(float(val) * 255) for val in parts)
                     light_intensity_flag = 0
                     continue
                 if light_transf_flag == 1 and '}' in parts:
                     light_transf_flag = 0
                     continue
-            # No need to print here
         return self.light_transf_index, self.light_intensity
     
     def display_light(self):
         light_transf_index, light_intensity = self.read_light_specifications()
-        print(light_transf_index, light_intensity)
+        print("Light: ",light_transf_index, light_intensity)
